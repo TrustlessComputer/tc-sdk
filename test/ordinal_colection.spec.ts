45b878ac95821ba1453ad6c2dbd91013fc6d6bb6
2525tc <2525@bvm.network>
2024-12-24 16:37:06 +0700

    update script test inscribe file

--- test/ordinal_colection.spec.ts
+++ test/ordinal_colection.spec.ts
@@ -31,7 +31,9 @@ import BigNumber from 'bignumber.js';
 import { assert } from 'chai';
 import { ethers } from "ethers";
 
-const fs = require('fs');
+// const fs = require('fs');
+
+const fs = require('fs').promises;
 
 require("dotenv").config({ path: __dirname + "/.env" });
 console.log(__dirname + "../test/.env");
@@ -49,6 +51,12 @@ let address2 = process.env.ADDRESS_2 || "";
 let privateKey2 = convertPrivateKeyFromStr(privateKeyWIF2);
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+const readFile = async (fileName: string): Promise<Buffer> => {
+    const data = await fs.readFile(__dirname + fileName);
+    console.log('File content:', data);
+    return data;
+}
 
 
 describe("Sign msg Tx", async () => {
@@ -89,29 +97,99 @@ describe("Sign msg Tx", async () => {
 
     // })
 
-    it("ORD inscribe parent inscription with meta protocol", async () => {
+    // it("ORD inscribe parent inscription with meta protocol", async () => {
+    //     // private key 2
+    //     let utxos: UTXO[] = [
+    //         {
+    //             tx_hash: "3fc9f3e95628397175c67297e6aff0e2756b9e6e3fc2b6e18975f8330e59b142",
+    //             tx_output_n: 1,
+    //             value: new BigNumber(88642)
+    //         }
+    //     ];
+
+    //     const metaProtocol = "BVM";
+    //     const parentInscTxID = "a885ff18ea446eda845acf8ae4bc9f8429c16a60fd1428f7344a284e3ef21a07";  // dont change
+    //     const parentInscTxIndex = 0;
+    //     const parentUTXO: UTXO = {
+    //         tx_hash: "92d6f930cca0874c26af389b788a28413732642333a1fc2c16c8ada6610d798f",
+    //         tx_output_n: 0,
+    //         value: new BigNumber(546),
+    //     }
+
+    //     const contentStr = ". BVM . The child inscription #0 ."
+    //     const contentType = "text/plain;charset=utf-8";
+
+    //     const contentBuffer = Buffer.from(contentStr, "utf-8");
+
+
+    //     const { commitTxHex, commitTxID, revealTxHex, revealTxID, totalFee } = await createInscribeTxGeneral({
+    //         senderPrivateKey: privateKey2,
+    //         senderAddress: address2,
+    //         utxos: utxos,
+    //         inscriptions: {},
+    //         feeRatePerByte: 5,
+    //         data: contentBuffer,
+    //         contentType,
+    //         metaProtocol,
+    //         parentInscTxID,
+    //         parentInscTxIndex,
+    //         parentUTXO,
+
+    //     });
+
+    //     console.log("commitTxHex: ", commitTxHex);
+    //     console.log("commitTxID: ", commitTxID);
+    //     console.log("revealTxHex: ", revealTxHex);
+    //     console.log("revealTxID: ", revealTxID);
+    //     console.log("totalFee: ", totalFee);
+
+    //     // await broadcastTx(commitTxHex);
+
+    //     // await sleep(3000);
+
+    //     // await broadcastTx(revealTxHex);
+
+    // });
+
+    // it("test something", async () => {
+    //     const resp = await fetch("https://ordinals.com/content/46b39a79e9087d4ceeed2b67bb5771a90ee6bd94f6aa75720732c355a7bd6e78i0");
+    //     console.log("Resp: ", resp);
+    //     console.log("Dataa:", await resp.text());
+
+    // })
+
+
+    it("ORD inscribe the root inscription", async () => {
         // private key 2
         let utxos: UTXO[] = [
             {
-                tx_hash: "3fc9f3e95628397175c67297e6aff0e2756b9e6e3fc2b6e18975f8330e59b142",
+                tx_hash: "2c9992307b250c0e7771196e439e01e754b616ec37278e4d42b16ad4d94c1f00",
                 tx_output_n: 1,
-                value: new BigNumber(88642)
+                value: new BigNumber(41264)
             }
         ];
 
-        const metaProtocol = "BVM";
-        const parentInscTxID = "a885ff18ea446eda845acf8ae4bc9f8429c16a60fd1428f7344a284e3ef21a07";  // dont change
-        const parentInscTxIndex = 0;
-        const parentUTXO: UTXO = {
-            tx_hash: "92d6f930cca0874c26af389b788a28413732642333a1fc2c16c8ada6610d798f",
-            tx_output_n: 0,
-            value: new BigNumber(546),
-        }
+        const metaProtocol = "Eternal AI";
+        // const parentInscTxID = "a885ff18ea446eda845acf8ae4bc9f8429c16a60fd1428f7344a284e3ef21a07";  // dont change
+        // const parentInscTxIndex = 0;
+        // const parentUTXO: UTXO = {
+        //     tx_hash: "92d6f930cca0874c26af389b788a28413732642333a1fc2c16c8ada6610d798f",
+        //     tx_output_n: 0,
+        //     value: new BigNumber(546),
+        // }
 
-        const contentStr = ". BVM . The child inscription #0 ."
-        const contentType = "text/plain;charset=utf-8";
+        // TODO: EDIT HERE
+        // const contentType = "text/javascript";
+        const contentType = "text/html";
+        // const contentBuffer = await readFile("./eternal/utils.js");
 
-        const contentBuffer = Buffer.from(contentStr, "utf-8");
+        // test / shardai / drawmin64"test/shardai/drawmin64"
+
+        // const contentType = "text/base64";
+        // const contentBuffer = await readFile("/shardai/final.js");
+        const contentBuffer = await readFile("/shardai/index.html");
+        console.log("contentBuffer: ", contentBuffer);
+        console.log("contentBuffer len: ", contentBuffer.length);
 
 
         const { commitTxHex, commitTxID, revealTxHex, revealTxID, totalFee } = await createInscribeTxGeneral({
@@ -119,13 +197,13 @@ describe("Sign msg Tx", async () => {
             senderAddress: address2,
             utxos: utxos,
             inscriptions: {},
-            feeRatePerByte: 5,
+            feeRatePerByte: 6,
             data: contentBuffer,
             contentType,
             metaProtocol,
-            parentInscTxID,
-            parentInscTxIndex,
-            parentUTXO,
+            // parentInscTxID,
+            // parentInscTxIndex,
+            // parentUTXO,
 
         });
 
@@ -135,11 +213,11 @@ describe("Sign msg Tx", async () => {
         console.log("revealTxID: ", revealTxID);
         console.log("totalFee: ", totalFee);
 
-        // await broadcastTx(commitTxHex);
+        await broadcastTx(commitTxHex);
 
-        // await sleep(3000);
+        await sleep(3000);
 
-        // await broadcastTx(revealTxHex);
+        await broadcastTx(revealTxHex);
 
     });
 });
